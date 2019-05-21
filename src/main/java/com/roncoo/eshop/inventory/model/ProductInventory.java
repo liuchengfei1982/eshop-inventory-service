ceecff035195157d084abe333a970ccc2ae48b9e
eagle <liuchengfei1982@126.com>
2019-05-21 21:14:23 +0800

    first commit

---
+++ src/main/java/com/roncoo/eshop/inventory/model/ProductInventory.java
@@ -0,0 +1,13 @@
+package com.roncoo.eshop.inventory.model;
+
+import lombok.Data;
+
+@Data
+public class ProductInventory {
+	
+	private Long id;
+	private Integer value;
+	private Long productId;
+	
+
+}
